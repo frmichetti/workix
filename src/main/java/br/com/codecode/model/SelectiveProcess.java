07032cad71d4ca6437eb24e06d276a62ab4fffeb
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2016-10-18 19:25:14 -0200

    min accept test ok

--- src/main/java/br/com/codecode/model/SelectiveProcess.java
+++ src/main/java/br/com/codecode/model/SelectiveProcess.java
@@ -1,6 +1,7 @@
 package br.com.codecode.model;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Observable;
@@ -11,83 +12,86 @@ import br.com.codecode.model.scaffold.Candidate;
 public class SelectiveProcess extends Observable implements Observer,Serializable  {
 
 	private static final long serialVersionUID = -8009482242264135346L;
-	
+
 	public Collection<Candidate> candidates = new ArrayList<>();	
-	
+
 	public boolean active;	
-	
+
 	public int maxCandidates;
-	
+
 	public SelectiveProcess() {
 		this.addObserver(this);	
 	}
-	
+
 	public boolean registerCandidate(Candidate candidate){
-		
+
 		boolean b = false;
-		
-		if(active){
-			System.out.println(candidate.getName() + "Registered with Success ");
-			candidates.add(candidate);
-			notifyChanges(candidates);
+
+		if(active && isElegible()){
+
+			candidates.add(candidate);			
+
+			System.out.println(candidate.getName() + " Registered with Success");
+
 			b = true;
+
 		}else{
-			System.out.println("Cannot Register - Process is Disabled");
+
+			System.out.println(candidate.getName() + " Cannot Register - Process is Disabled");
+
 			b = false;
 		}
+
+		notifyChanges(candidates);
+
 		return b;
-				
+
 	}
 
 
 	@Override
 	public void update(Observable observable, Object object) {		
-		
-		System.out.println("[Update]");	
-		
-		
-		if(observable instanceof SelectiveProcess){		
-			
-			System.out.println("[Observable instanceof Selective Process]");
-			
-			if(maxCandidates == candidates.size()){
-			
-				System.out.println("[maxCandidates == candidates.size()]");
-				
-				active = false;
-				
-				System.out.println("Max candidates Reached - Disabled Process");
-				
-				System.out.println("Process " + ((active) ? "Active" : "Disabled" ));
-				
-				if(object instanceof ArrayList<?>){
-					
-					System.out.println("Object instanceof ArrayList<?>");			
-					
-					maxCandidates = ((ArrayList<?>) object).size();					
-					
-					System.out.println("Registered Candidates = " + maxCandidates);
+
+		if(observable instanceof SelectiveProcess){			
+
+			if(active = isElegible()){
+
+				if(object instanceof ArrayList<?>){								
+
+					countCandidates((ArrayList<?>) object);
+
 				}
-				
+
+			}else{
+				System.out.println("Max candidates Reached - Disabled Process at " + Instant.now());
 			}
-			
-			
+
+
 		}
-		
+
 	}
-	
-	private void notifyChanges(){
-		System.out.println("[Notify Changes]");
+
+	private boolean isElegible(){
+		System.out.println("[" + candidates.size() + "/" + maxCandidates+ "]");
+		return (candidates.size() < maxCandidates);
+	}
+
+	private void countCandidates(Collection<?> collection){		
+		maxCandidates = collection.size();
+		System.out.println("Registered Candidates = " + maxCandidates);
+	}
+
+
+	private void notifyChanges(){		
 		notifyObservers();
 		setChanged();
 	}
-	
-	private void notifyChanges(Object object){
-		System.out.println("[Notify Changes]");
+
+	private void notifyChanges(Object object){		
 		notifyObservers(object);
 		setChanged();
 	}
-	
-	
+
+
 
 }
--- src/test/java/br/com/codecode/model/TestSelectiveProcess.java
+++ src/test/java/br/com/codecode/model/TestSelectiveProcess.java
@@ -14,7 +14,7 @@ public class TestSelectiveProcess {
 	private SelectiveProcess selectiveProcess;
 
 	private ArrayList<Candidate> candidates;
-	
+
 	private Candidate c1,c2,c3;
 
 	@Before
@@ -22,9 +22,9 @@ public class TestSelectiveProcess {
 
 		selectiveProcess = new SelectiveProcess();
 
-		selectiveProcess.active = true;
+		selectiveProcess.active = true;		
 		
-		selectiveProcess.maxCandidates = 2;
+		selectiveProcess.maxCandidates = 1 ;
 
 		c1 = new Candidate();
 		c2 = new Candidate();
@@ -36,13 +36,13 @@ public class TestSelectiveProcess {
 		candidates.add(c2);
 		candidates.add(c3);
 
-		
+
 		for(int x=0 ; x < candidates.size() ;x++){
 			candidates.get(x).setId(x+1L);
 			candidates.get(x).setName("Candidate " + (x+1L));
 		}
 
-		
+
 	}
 
 	@Test
@@ -52,16 +52,35 @@ public class TestSelectiveProcess {
 
 		assertNotNull(candidates);
 
-		assertEquals(3, candidates.size());
+		assertEquals(1, selectiveProcess.maxCandidates);
 		
-		assertNotEquals(0, selectiveProcess.maxCandidates );
+		assertTrue(selectiveProcess.active);
+		
+		assertEquals(3, candidates.size());
+
+		
 	}
 
 	@Test
-	public void testRegisterCandidates(){		
+	public void testRegisterTwoCandidates(){	
+		
+		selectiveProcess.maxCandidates = 2;
+
+		assertTrue(selectiveProcess.active);
+
 		assertTrue(selectiveProcess.registerCandidate(c1));
-		assertTrue(selectiveProcess.registerCandidate(c2));
+
+		assertTrue(selectiveProcess.active);
+
+		assertTrue(selectiveProcess.registerCandidate(c2));		
+
+		assertFalse(selectiveProcess.active);
+		
 		assertFalse(selectiveProcess.registerCandidate(c3));
+
+		assertFalse(selectiveProcess.active);
 	}
+
+	
 
 }
