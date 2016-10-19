8e37a389676892d4ea243af0939bacf256e49243
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2016-10-19 03:58:20 -0200

    home page added

---
+++ src/main/java/br/com/codecode/bean/HomeMB.java
@@ -0,0 +1,74 @@
+package br.com.codecode.bean;
+
+import java.util.Collection;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+
+import br.com.codecode.dao.CandidateDao;
+import br.com.codecode.dao.EmployeerDao;
+import br.com.codecode.dao.JobDao;
+import br.com.codecode.model.scaffold.Job;
+
+@Model
+public class HomeMB {
+	
+	@Inject
+	private JobDao jobDao;
+	
+	@Inject
+	private EmployeerDao empDao;
+	
+	@Inject
+	private CandidateDao candDao;
+	
+	private Collection<Job> joblist;
+	
+	private int counterJobs;
+	
+	private int counterCandidates;
+	
+	private int counterEmployeers;	
+	
+	private int counterResumes;
+	
+	public int getCounterJobs() {
+		return counterJobs;
+	}
+
+	public int getCounterCandidates() {
+		return counterCandidates;
+	}
+
+	public int getCounterEmployeers() {
+		return counterEmployeers;
+	}
+
+	public int getCounterResumes() {
+		return counterResumes;
+	}
+
+	public HomeMB() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@PostConstruct
+	private void init(){
+		
+		joblist = jobDao.listAll(0, 100);
+		
+		counterJobs =  jobDao.listAll(0, Integer.MAX_VALUE).size();
+		
+		counterEmployeers =  empDao.listAll(0, Integer.MAX_VALUE).size();
+		
+		counterCandidates = candDao.listAll(0, Integer.MAX_VALUE).size();
+		
+		
+	}
+
+	public Collection<Job> getJoblist() {
+		return joblist;
+	}
+
+}
