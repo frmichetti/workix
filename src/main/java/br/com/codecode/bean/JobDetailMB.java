f01afc39897cfff435b30bb6949db00334fa6758
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2016-10-19 04:42:59 -0200

    added detail page

---
+++ src/main/java/br/com/codecode/bean/JobDetailMB.java
@@ -0,0 +1,63 @@
+package br.com.codecode.bean;
+
+import java.math.BigDecimal;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+
+import br.com.codecode.dao.JobDao;
+import br.com.codecode.model.scaffold.Job;
+
+@Model
+public class JobDetailMB {
+	
+	@Inject
+	private JobDao dao;
+	
+	private Long id = 0L;
+	
+	private Job currentJob ;
+	
+	public JobDetailMB() {
+	
+	}
+
+	public void init(){
+		
+		System.out.println("JOB ID RECEIVED -> " + id);
+		
+		if(id > 0){
+			currentJob = dao.findById(id);	
+		}		
+		
+		if (currentJob == null){
+			currentJob = new Job();
+			currentJob.setId(0L);
+			currentJob.setTitle("Não Encontrado");			
+			currentJob.setAverageSalary(new BigDecimal(0));
+		}
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long jobId) {
+		this.id = jobId;
+	}
+
+	public Job getCurrentJob() {
+		return currentJob;
+	}
+
+	public void setCurrentJob(Job currentJob) {
+		this.currentJob = currentJob;
+	}
+	
+	
+	
+	
+
+}
