6d738b4fd153641e64a7fa75a159eab46bd9cfb6
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2016-10-20 20:00:06 -0200

    fix navigation

--- src/main/java/br/com/codecode/akijob/bean/NavigationMB.java
+++ src/main/java/br/com/codecode/akijob/bean/NavigationMB.java
@@ -1,5 +1,6 @@
 package br.com.codecode.akijob.bean;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
@@ -10,42 +11,53 @@ import javax.inject.Named;
 @ManagedBean
 @Named
 public class NavigationMB {
-	
+
 	@Inject
 	private FacesContext facesContext;
-	
+
 	private String prefix;
-	
+
 	private String sufix; 	
-	
+
 	public NavigationMB() {
-		prefix = facesContext.getExternalContext().getContextName();
+
+	}
+
+	@PostConstruct
+	private void init(){
+		prefix = "/" + facesContext.getExternalContext().getContextName();
 		sufix = "?faces-redirect=true";
 	}
-	
+
 	public String goToIndex(){
+		System.out.println("Redirect to -> " + prefix + "/index.xhtml" + sufix );
 		return prefix + "/index.xhtml" + sufix;
 	}
-	
+
 	public String goToJobs(){
+		System.out.println("Redirect to -> " + prefix + "/index.xhtml" + sufix );
 		return prefix + "/jobs.xhtml" + sufix;
 	}
-	
+
 	public String goToPostAJob(){
+		System.out.println("Redirect to -> " + prefix + "/index.xhtml" + sufix );
 		return prefix + "/post-a-job.xhtml" + sufix;
 	}
-	
+
 	public String goToCandidates(){
+		System.out.println("Redirect to -> " + prefix + "/index.xhtml" + sufix );
 		return prefix + "/candidates.xhtml" + sufix;
 	}
-	
-	
+
+
 	public String goToPostAResume(){
+		System.out.println("Redirect to -> " + prefix + "/index.xhtml" + sufix );
 		return prefix + "/post-a-resume.xhtml" + sufix;
 	}
 
-	
+
 	public String goToAboutUs(){
+		System.out.println("Redirect to -> " + prefix + "/index.xhtml" + sufix );
 		return prefix + "/about.xhtml" + sufix;
 	}
 
