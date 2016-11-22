38d50ad8380c5c1c389f5bd685ca5853dd1e2b45
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2016-11-22 16:42:35 -0200

    Update Packages

--- src/main/java/br/com/codecode/workix/infra/MailBodyBuilder.java
+++ src/main/java/br/com/codecode/workix/infra/MailBodyBuilder.java
@@ -5,29 +5,52 @@
  * @see http://www.codecode.com.br
  * @see mailto:frmichetti@gmail.com
  * */
-package br.com.codecode.workix.util;
+package br.com.codecode.workix.infra;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+
+import br.com.codecode.workix.cdi.qualifier.Factory;
+import br.com.codecode.workix.exception.NotImplementedYetException;
 import br.com.codecode.workix.model.scaffold.Candidate;
 import br.com.codecode.workix.model.scaffold.Job;
+import br.com.codecode.workix.util.ReadFile;
 
-
+/**
+ * This Class Construct Email Body from a Template
+ * TODO FIXME
+ * @author felipe
+ *
+ */
 public class MailBodyBuilder {
+	
+	@Inject @Factory
+	private FacesContext facesContext;
 
-	private static final String LOCAL_SERVER = "http://localhost:8080/jobs";
+	private String server, templatePath;
+	
+	public MailBodyBuilder() throws NotImplementedYetException{
+		throw new NotImplementedYetException();
+	}
+	
+	@PostConstruct
+	private void init(){
+		server = facesContext.getExternalContext().getApplicationContextPath();
+	}
 
-//	private static final String REMOTE_SERVER = "http://powerofjava.frmichetti.rhcloud.com";	
 
-	public static String makeBodyForNewCheckout(Job job) {
+	public String makeBodyForNew(Job job) {
 
 		StringBuilder mailBody = new StringBuilder();
 
 		try {
 
 			mailBody.append(ReadFile.readFileFromURL(
-					new URL(LOCAL_SERVER + "/mail/email_template_nova_compra.html")));
+					new URL(server + templatePath)));
 
 			String result = mailBody.toString();
 
@@ -53,21 +76,21 @@ public class MailBodyBuilder {
 
 	}
 
-	public static String makeBodyForNewUser(Candidate candidate) {
+	public String makeBodyForNew(Candidate candidate) {
 
 		StringBuilder mailBody = new StringBuilder();	
 
 		try {
 
 			mailBody.append(ReadFile.readFileFromURL(
-					new URL(LOCAL_SERVER + "/mail/email_template_bem_vindo.html")));
+					new URL(server + templatePath)));
 
 			String result = mailBody.toString();
 
 			mailBody = new StringBuilder();
-			
+
 			result = result.replace("varNome", candidate.getName());
-			
+
 			result = result.replace("varUsuario", candidate.getUser().getEmail());
 
 			mailBody.append(result);	
