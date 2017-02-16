af2a508dfff289af3a8f6ab61d4759dd588d42d1
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2017-02-16 20:22:44 -0200

    Fix Testimonials Fragments

---
+++ src/main/java/br/com/codecode/workix/beans/TestimonialsMB.java
@@ -0,0 +1,54 @@
+package br.com.codecode.workix.beans;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+import javax.inject.Inject;
+
+import br.com.codecode.workix.cdi.dao.Crud;
+import br.com.codecode.workix.cdi.qualifiers.Generic;
+import br.com.codecode.workix.core.exceptions.NotImplementedYetException;
+import br.com.codecode.workix.jpa.models.Testimonial;
+
+/**
+ * This ManagedBean controls Testimonials Fragment on HomePage
+ * 
+ * @author felipe
+ * @since 1.0
+ * @version 1.1
+ * @see BaseMB
+ */
+@Model
+public class TestimonialsMB extends BaseMB {
+    
+    @Inject
+    @Generic
+    private Crud<Testimonial> testDao;
+    
+    private DataModel<Testimonial> list;
+
+    @PostConstruct
+    @Override
+    protected void init() {
+	try {
+	    
+	    list = new ListDataModel<Testimonial>(testDao.listAll(0, Integer.MAX_VALUE));
+	    
+	} catch (NotImplementedYetException e) {	    
+	    e.printStackTrace();
+	}	
+
+    }
+
+    
+    /**
+     * @return the list
+     */
+    public DataModel<Testimonial> getList() {
+        return list;
+    }
+    
+    
+
+}
