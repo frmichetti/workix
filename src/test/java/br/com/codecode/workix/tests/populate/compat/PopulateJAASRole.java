7695b495c018271cf7db655ee0fd13f47b2c1aa0
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2017-02-24 00:17:01 -0300

    Added new Tests

---
+++ src/test/java/br/com/codecode/workix/tests/populate/compat/PopulateJAASRole.java
@@ -0,0 +1,83 @@
+/**
+ *
+ * @author Felipe Rodrigues Michetti
+ * @see http://portfolio-frmichetti.rhcloud.com
+ * @see http://www.codecode.com.br
+ * @see mailto:frmichetti@gmail.com
+ * */
+package br.com.codecode.workix.tests.populate.compat;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.codecode.workix.jaas.models.JAASRole;
+import br.com.codecode.workix.tests.android.BaseTest;
+import br.com.codecode.workix.tests.util.HttpTest;
+
+/**
+ * Populate DB with JAAS Roles
+ * 
+ * @author felipe
+ * @since 1.1
+ * @version 1.0
+ */
+public class PopulateJAASRole extends BaseTest {
+
+    private String resp;
+
+    private List<JAASRole> roles;    
+
+    private void addToList(JAASRole role) {
+
+	assertNotNull(role);
+
+	System.out.println("[addToList] " + role.getName());
+
+	roles.add(role);
+
+    }
+
+
+    @Before    
+    public void create() {
+
+	roles = new ArrayList<>();	
+
+	JAASRole admin = JAASRole.builder().withName("Admin").build();
+
+	JAASRole guest = JAASRole.builder().withName("Guest").build();
+
+	System.out.println("[create] " + admin.getName());
+
+	addToList(admin);		
+
+	System.out.println("[create] " + guest.getName());	
+	
+	addToList(guest);
+
+	assertTrue(roles.size() > 0 );
+
+    }
+
+    @Test
+    public void sendToServer() {
+
+	roles.stream().forEach(r -> {
+
+	    System.out.println("[sendToServer] " + r.getName());
+
+	    resp = HttpTest.sendPost(server + "/jaasroles", getGson().toJson(r));
+
+	    assertNotNull(resp);
+
+	});
+
+    }
+
+}
---
+++ src/test/java/br/com/codecode/workix/tests/populate/compat/PopulateSelectiveProcessTest.java
@@ -0,0 +1,112 @@
+package br.com.codecode.workix.tests.populate.compat;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.gson.reflect.TypeToken;
+
+import br.com.codecode.workix.jpa.models.compat.Job;
+import br.com.codecode.workix.jpa.models.compat.SelectiveProcess;
+import br.com.codecode.workix.tests.android.BaseTest;
+import br.com.codecode.workix.tests.populate.CommonPopTest;
+import br.com.codecode.workix.tests.util.HttpTest;
+
+/**
+ * Populate DB with Selective Processes
+ * 
+ * @since 1.0
+ * @version 1.0
+ * @author felipe
+ */
+public class PopulateSelectiveProcessTest extends BaseTest implements CommonPopTest<SelectiveProcess> {
+
+    private String resp;
+
+    private List<Job> jobs;
+
+    private List<SelectiveProcess> processes;
+
+    private int howManyProcesses = 50;
+
+    @Before
+    public void downloadJobs() {
+
+	System.out.println("[downloadJobs]");
+
+	resp = HttpTest.sendGet(server + "/jobs");
+
+	jobs = getGson().fromJson(resp, new TypeToken<List<Job>>() {
+	}.getType());
+
+	assertNotNull(jobs);
+
+	assertTrue(jobs.size() > 0);
+
+    }
+
+    @Override
+    public void addToList(SelectiveProcess selectiveProcess) {
+
+	assertNotNull(selectiveProcess);
+
+	assertNotNull(processes);
+
+	System.out.println("[addToList]" + selectiveProcess.getJob().getTitle());
+
+	processes.add(selectiveProcess);
+    }
+
+    @Override
+    public void create() {
+
+	assertNotNull(jobs);
+
+	assertTrue(jobs.size() > 0);
+
+	processes = new ArrayList<>();
+
+	for (int x = 0; x < howManyProcesses; x++) {
+
+	    SelectiveProcess sp = new SelectiveProcess();
+
+	    sp.setJob(jobs.get(x));
+
+	    sp.setMaxCandidates(1);
+
+	    addToList(sp);
+	}
+
+	assertEquals(50, processes.size());
+
+    }
+
+    @Test
+    @Override
+    public void sendToServer() {
+
+	create();
+
+	assertNotNull(jobs);
+
+	assertTrue(jobs.size() > 0);
+
+	processes.forEach(p -> {
+
+	    System.out.println("[sendToServer] " + p.getJob().getTitle());
+
+	    resp = HttpTest.sendPost(server + "/selectiveprocesses", getGson().toJson(p));
+
+	    assertNotNull(resp);
+
+	});
+
+    }
+
+}
