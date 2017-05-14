1d30193022e79cfcee4a7a03feb96a72bbe52cc6
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2017-05-14 17:23:30 -0300

    Cleanup

--- src/main/resources/br/com/codecode/workix/security/JAASBase_.java
+++
@@ -1,9 +0,0 @@
-package br.com.codecode.workix.security;
-
-import javax.annotation.Generated;
-import javax.persistence.metamodel.StaticMetamodel;
-
-@Generated(value="Dali", date="2016-11-29T17:59:20.474-0200")
-@StaticMetamodel(JAASBase.class)
-public class JAASBase_ {
-}
