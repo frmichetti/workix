62118855d978f6cd6fdb69fa7aca9fc6150c3443
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2019-05-09 06:55:12 -0300

    Added DTO for User

--- src/main/java/br/com/codecode/workix/dto/User.java
+++ src/main/java/br/com/codecode/workix/dto/User.java
@@ -1,30 +1,9 @@
-package br.com.codecode.workix.jpa.models;
+package br.com.codecode.workix.dto;
 
-import br.com.codecode.workix.cdi.qualifiers.Persist;
 import br.com.codecode.workix.interfaces.Buildable;
 import br.com.codecode.workix.interfaces.Notificable;
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotEmpty;
 
-import javax.persistence.*;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 
-/**
- * User JPA with Inherited Fields and Methods
- * No Anotation for Compatibility Only with Older Versions
- * @author felipe
- * @since 1.0
- * @version 1.1
- * @see MyEntity
- * @see Notificable
- */
-@Entity
-@XmlRootElement
-@XmlAccessorType(XmlAccessType.PUBLIC_MEMBER)
-@Persist
 public class User extends MyEntity implements Notificable {
 
 	private static final long serialVersionUID = -610648880358327958L;
@@ -60,42 +39,30 @@ public class User extends MyEntity implements Notificable {
 	 *
 	 * @return created builder
 	 */
-	@XmlTransient
 	public static Builder builder() {
 		return new Builder();
 	}
 
-	@NotEmpty
-	@Email
-	@Column(nullable = false, unique = true)
 	@Override
 	public String getEmail() {
 		return email;
 	}
 
-	@NotEmpty
-	@Column
 	@Override
 	public String getFirebaseMessageToken() {
 		return firebaseMessageToken;
 	}
 
-	@NotEmpty
-	@Column
 	@Override
 	public String getFirebaseUUID() {
 		return firebaseUUID;
 	}
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(updatable = false, nullable = false)
 	@Override
 	public long getId() {
 		return this.id;
 	}
 
-	@Column
 	public boolean isActive() {
 		return active;
 	}
