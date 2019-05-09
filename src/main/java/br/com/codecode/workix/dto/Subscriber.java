5949f4a8fda819a709b68c42a89d5a802f7d44d3
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2019-05-09 07:03:05 -0300

    Added DTO for Subscriber

--- src/main/java/br/com/codecode/workix/dto/Subscriber.java
+++ src/main/java/br/com/codecode/workix/dto/Subscriber.java
@@ -1,27 +1,8 @@
-package br.com.codecode.workix.jpa.models;
+package br.com.codecode.workix.dto;
 
-import br.com.codecode.workix.cdi.qualifiers.Persist;
 import br.com.codecode.workix.interfaces.Buildable;
-import org.hibernate.validator.constraints.NotEmpty;
 
-import javax.persistence.*;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 
-/**
- * Subscriber JPA with Inherited Fields and Methods
- * No Anotation for Compatibility Only with Older Versions
- * @author felipe
- * @since 1.0
- * @version 1.1
- * @see MyEntity
- */
-@Entity
-@XmlRootElement
-@XmlAccessorType(XmlAccessType.PUBLIC_MEMBER)
-@Persist
 public class Subscriber extends MyEntity {
 
 	private static final long serialVersionUID = 6675137603968146834L;
@@ -50,7 +31,6 @@ public class Subscriber extends MyEntity {
 	 * Creates builder to build {@link Subscriber}.
 	 * @return created builder
 	 */
-	@XmlTransient
 	public static Builder builder() {
 		return new Builder();
 	}
@@ -58,15 +38,10 @@ public class Subscriber extends MyEntity {
 	/**
 	 * @return the email
 	 */
-	@NotEmpty
-	@Column
 	public String getEmail() {
 		return email;
 	}
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(updatable = false, nullable = false)
 	@Override
 	public long getId() {
 		return this.id;
