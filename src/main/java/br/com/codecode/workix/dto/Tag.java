ca8b80993c3d132b70172e52404e1bc41c486693
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2019-05-09 07:02:01 -0300

    Added DTO for Tag

--- src/main/java/br/com/codecode/workix/dto/Tag.java
+++ src/main/java/br/com/codecode/workix/dto/Tag.java
@@ -1,113 +1,92 @@
-package br.com.codecode.workix.jpa.models;
+package br.com.codecode.workix.dto;
 
-import br.com.codecode.workix.cdi.qualifiers.Persist;
 import br.com.codecode.workix.interfaces.Buildable;
-import org.hibernate.validator.constraints.NotEmpty;
 
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 import java.io.Serializable;
 
-/**
- * Tag JPA Embeddable
- * No Anotation for Compatibility Only with Older Versions
- * @author felipe
- * @since 1.0
- * @version 1.1
- * @see Serializable
- */
-@Embeddable
-@XmlRootElement
-@XmlAccessorType(XmlAccessType.PUBLIC_MEMBER)
-@Persist
-public class Tag implements Serializable{
+public class Tag implements Serializable {
 
-	private static final long serialVersionUID = 323076947054044016L;
+    private static final long serialVersionUID = 323076947054044016L;
 
-	private String name;
+    private String name;
 
-	/**
-	 * Public Default Constructor for JPA Compatibility Only
-	 */
-	public Tag(){}
+    /**
+     * Public Default Constructor for JPA Compatibility Only
+     */
+    public Tag() {
+    }
 
-	/**
-	 * Public Constructor for {@link Builder} Compatibility
-	 *
-	 * @see Buildable
-	 * @param builder
-	 *            Builder for Generate a New Tag
-	 */
-	private Tag(Builder builder) {
-		this.name = builder.getName();
-	}
+    /**
+     * Public Constructor for {@link Builder} Compatibility
+     *
+     * @param builder Builder for Generate a New Tag
+     * @see Buildable
+     */
+    private Tag(Builder builder) {
+        this.name = builder.getName();
+    }
 
-	/**
-	 * @return the name
-	 */
-	@NotEmpty
-	@Column
-	public String getName() {
-		return name;
-	}
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
 
 
-	/**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	@Override
-	public String toString() {
-		return this.name;
-	}
+    @Override
+    public String toString() {
+        return this.name;
+    }
 
-	/**
-	 * Creates builder to build {@link Tag}.
-	 * @return created builder
-	 */
-	@XmlTransient
-	public static Builder builder() {
-		return new Builder();
-	}
+    /**
+     * Creates builder to build {@link Tag}.
+     *
+     * @return created builder
+     */
+    public static Builder builder() {
+        return new Builder();
+    }
 
 
-	/**
-	 * Builder NestedClass for {@link Tag}
-	 *
-	 * @author felipe
-	 * @since 1.0
-	 * @version 1.0
-	 * @see Tag
-	 * @see Buildable
-	 */
-	public static final class Builder extends Tag implements Buildable<Tag> {
+    /**
+     * Builder NestedClass for {@link Tag}
+     *
+     * @author felipe
+     * @version 1.0
+     * @see Tag
+     * @see Buildable
+     * @since 1.0
+     */
+    public static final class Builder extends Tag implements Buildable<Tag> {
 
-		private static final long serialVersionUID = -5775038446937981944L;
+        private static final long serialVersionUID = -5775038446937981944L;
 
-		/**
-		 * Disabled Empty Constructor
-		 */
-		private Builder(){}
+        /**
+         * Disabled Empty Constructor
+         */
+        private Builder() {
+        }
 
-		public Builder withName(String name) {
-			super.name = name;
-			return this;
-		}
+        public Builder withName(String name) {
+            super.name = name;
+            return this;
+        }
 
-		/**
-		 * Return a New Tag
-		 */
-		@Override
-		public Tag build() {
-			return new Tag(this);
-		}
-	}
+        /**
+         * Return a New Tag
+         */
+        @Override
+        public Tag build() {
+            return new Tag(this);
+        }
+    }
 
 }
